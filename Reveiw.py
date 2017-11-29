db6322e38d74f407e0d7fcfd7326b672c77de8aa
ZiqianPei <32119172+ZiqianPei@users.noreply.github.com>
2017-11-28 20:02:58 -0800

    Update Reveiw.py

--- Reveiw.py
+++ Reveiw.py
@@ -6,7 +6,8 @@ html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
 tags = soup('span')
 for tag in tags:
-    raw_info = str(tag)
-    review_info = re.findall('\S+%', raw_info) 
-    review_info += re.findall('\S+,\S+',raw_info)
-    print (review_info)
+    if tag.get('class',[]) == ['nonresponsive_hidden', 'responsive_reviewdesc']:
+        raw_info = str(tag)
+        review_info = re.findall('\S+%', raw_info) 
+        review_info += re.findall('\S+,\S+',raw_info)
+        print (review_info)
